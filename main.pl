3e8ee9ca5b2ba7ab25e0bc4754988df6133c63b6
Vladimir Kidyaev <krios2146@gmail.com>
2025-04-19 19:51:14 +0700

    feat(api): add `/exchangeRates` rout with real data fetched from DB

--- main.pl
+++ main.pl
@@ -37,6 +37,60 @@ sub find_all_currencies {
   return (undef, $currencies);
 }
 
+sub find_all_exchange_rates {
+  my $exchage_rates;
+
+  eval {
+    my $query = '
+    SELECT 
+      e.id AS id,
+      b.id AS b_id, b.code AS b_code, b.full_name AS b_full_name, b.sign AS b_sign,
+      t.id AS t_id, t.code AS t_code, t.full_name AS t_full_name, t.sign AS t_sign,
+      e.rate AS rate
+    FROM Exchange_rates e
+      JOIN Currencies b ON e.base_currency_id = b.id
+      JOIN Currencies t ON e.target_currency_id = t.id';
+
+    my $statement = $db_connection->prepare($query);
+    $statement->execute();
+
+    my @rows;
+    while (my $row = $statement->fetchrow_hashref()) {
+      push(@rows, $row);
+    }
+
+    @rows = [
+      map { 
+        { 
+          id => $_->{id},
+          rate => $_->{rate},
+          baseCurrency => {
+              id   => $_->{b_id},
+              name => $_->{b_full_name},
+              code => $_->{b_code},
+              sign => $_->{b_sign},
+          },
+          targetCurrency => {
+              id   => $_->{t_id},
+              name => $_->{t_full_name},
+              code => $_->{t_code},
+              sign => $_->{t_sign},
+          }
+        } 
+      } @rows 
+    ];
+
+    $exchage_rates = \@rows;
+  };
+
+  if ($@) {
+    app->log->error("Database call error: $@");
+    return (DBI::errstr, undef);
+  }
+
+  return (undef, $exchage_rates);
+}
+
 get '/currencies' => sub ($c) {
   my ($err, $currencies) = find_all_currencies();
 
@@ -60,6 +114,27 @@ get '/currencies' => sub ($c) {
   }
 
   $c->render(json => $currencies);
+};
+
+get '/exchangeRates' => sub ($c) {
+  my ($err, $exchange_rates) = find_all_exchange_rates();
+
+  if (defined $err) {
+    $c->app->log->error("Error while fetching exchange rates from DB: $err");
+
+    my $error_response = {
+      error   => 'Error reading exchange rates', 
+      message => $err
+    };
+
+    $c->render(json => $error_response);
+
+    return;
+  }
+
+  $c->app->log->info("Found " . scalar(@$exchange_rates) . " exchange rates");
+
+  $c->render(json => $exchange_rates);
 };
 
 app->start;
