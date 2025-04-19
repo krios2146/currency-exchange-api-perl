2892e0bffdaaa59c9c93833f36f179ebe7ca07cd
Vladimir Kidyaev <krios2146@gmail.com>
2025-04-19 19:08:56 +0700

    feat(api): add `/currencies` route with real data fetched from DB

--- main.pl
+++ main.pl
@@ -1,21 +1,65 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
+use DBI;
+use DBD::SQLite::Constants ':dbd_sqlite_string_mode';
 
-get '/' => sub ($c) {
-  $c->render(template => 'index');
+my $data_source = 'DBI:SQLite:dbname=database.sqlite';
+my $username = '';
+my $password = '';
+my $attr = {
+  RaiseError => 1, 
+  PrintError => 0,
+  sqlite_string_mode => DBD_SQLITE_STRING_MODE_UNICODE_STRICT 
+};
+
+our $db_connection = DBI->connect($data_source, $username, $password, $attr) or die $DBI::errstr;
+
+sub find_all_currencies {
+  my $currencies;
+
+  eval {
+    my $statement = $db_connection->prepare('SELECT * FROM Currencies');
+    $statement->execute();
+
+    my @rows;
+    while (my $row = $statement->fetchrow_hashref()) {
+      push(@rows, $row);
+    }
+
+    $currencies = \@rows;
+  };
+
+  if ($@) {
+    app->log->error("Database call error: $@");
+    return (DBI::errstr, undef);
+  }
+
+  return (undef, $currencies);
+}
+
+get '/currencies' => sub ($c) {
+  my ($err, $currencies) = find_all_currencies();
+
+  if (defined $err) {
+    $c->app->log->error("Error while fetching currencies from DB: $err");
+
+    my $error_response = {
+      error   => 'Error reading currencies', 
+      message => $err
+    };
+
+    $c->render(json => $error_response);
+
+    return;
+  }
+
+  $c->app->log->info("Found " . scalar(@$currencies) . " currencies");
+
+  foreach my $currency (@$currencies) {
+    $currency->{name} = delete $currency->{full_name};
+  }
+
+  $c->render(json => $currencies);
 };
 
 app->start;
-__DATA__
-
-@@ index.html.ep
-% layout 'default';
-% title 'Welcome';
-<h1>Welcome to the Mojolicious real-time web framework!</h1>
-
-@@ layouts/default.html.ep
-<!DOCTYPE html>
-<html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
-</html>
